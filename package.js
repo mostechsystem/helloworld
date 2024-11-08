e09640fca9819504aa14b88a789448c8ffef6303
monwuzua <monwuzua@appzonegroup.com>
2024-11-08 04:11:28 +0100

    added files

---
+++ package.js
@@ -0,0 +1,15 @@
+{
+    "name"; "sample-node-app",
+    "version"; "1.0.0",
+    "description"; "A simple Node.js app to be containerized",
+    "main"; "index.js",
+    "scripts"; {
+      "start"; "node index.js"
+    }
+    "dependencies"; {
+      "express"; "^4.18.2"
+    }
+    "author"; "moses onwuzua",
+    "license"; "ISC"
+}
+  
